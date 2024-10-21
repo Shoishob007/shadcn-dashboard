9ae5f6e22f0caab5115ed8bc6f5e6d3c783bfdac
Shoishob007 <u1704049@student.cuet.ac.bd>
2024-10-21 15:45:56 +0600

    validated job creating form

---
+++ src/app/(dashboard)/schemas/jobFormSchema.js
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const jobSchema = z.object({
+    title: z.string().min(2, "Put a valid job title"),
+    designation: z.string().min(2, "Put a valid job designation"),
+    description: z.string().min(10, "Put a valid job description"),
+    requirements: z.string().min(8, "Put valid requirements"),
+    jobCategory: z.string(),
+    jobType: z.string(),
+    salaryRange: z.number().min(3, "Salary range is required"),
+    deadline: z.string().optional(),
+});
