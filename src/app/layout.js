a1a2a46c5771e4353d63aa8679c6480effb75393
emamjion <emamjion@gmail.com>
2024-10-22 12:22:14 +0600

    applicant sidebar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,44 @@
 /** @format */
 "use client";
 
-import "./globals.css";
-import { cn } from "../lib/utils";
+import { role } from "@/components/RoleManagement";
 import SideNavbar from "@/components/SideNavbar";
-import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/toaster";
 import VerticalNavbar from "@/components/VerticalNavbar";
+import { SessionProvider } from "next-auth/react";
+import { cn } from "../lib/utils";
+import "./globals.css";
+
 
 export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       <body className={cn("min-h-screen w-full bg-gray-100 text-black flex")}>
-        <SessionProvider session={session}>
+        {role === 'applicant' ? (
+          <SessionProvider session={session}>
+          {/* SideNavbar */}
+          <SideNavbar/>
+          
+          {/* Vertical Navbar */}
+          <div className="flex flex-col w-full min-h-screen">
+            <VerticalNavbar/>
+
+            {/* content area */}
+            <div className="flex-grow p-2 rounded-lg ml-2 mr-4">{children}</div>
+          </div>
+          <Toaster />
+        </SessionProvider>
+        ) : (
+          <SessionProvider session={session}>
           <SideNavbar />
           <div className="flex flex-col w-full min-h-screen">
             <VerticalNavbar className="p-4" />
             <div className="flex-grow p-2 rounded-lg ml-2 mr-4">{children}</div>
           </div>
-
           <Toaster />
         </SessionProvider>
+        )}
+
       </body>
     </html>
   );
