dc36d7c2f74b1a168634717e126441eb7bfaf4f0
emamjion <emamjion@gmail.com>
2024-10-31 07:26:28 +0600

    I added page title in some page

--- src/app/(main-dashboard)/(applicantDashboard)/profile/page.jsx
+++ src/app/(main-dashboard)/(applicantDashboard)/profile/page.jsx
@@ -1,7 +1,15 @@
+"use client"
+import FormatTitle from "@/components/TitleFormatter";
+import { usePathname } from "next/navigation";
 
 const Profile = () => {
+    const pathname = usePathname();
+    const pageTitle = FormatTitle(pathname);
+    
     return (
-        <div></div>
+        <div>
+             <PageTitle title={pageTitle} className={"pb-4 ml-2"} />
+        </div>
     );
 };
 
