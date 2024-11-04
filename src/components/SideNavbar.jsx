52b2489bc75b50ba7c58ee9c8ed87c724185c4e3
Shoishob007 <u1704049@student.cuet.ac.bd>
2024-11-04 12:48:19 +0600

    sidenavbar tooltip changed

--- src/components/SideNavbar.jsx
+++ src/components/SideNavbar.jsx
@@ -9,13 +9,19 @@ import useRoleStore from "@/stores/roleStore/useRoleStore";
 import { SidebarLinks } from "./SidebarLinks";
 import { LogOut } from "lucide-react";
 import { Button } from "./ui/button";
- 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export default function SideNavbar() {
   const { status, data: session } = useSession();
   const { isCollapsed } = useLayoutStore();
   const { currentRole } = useRoleStore(); // Access the current role from Zustand store
   const { applicantLinks, organizationLinks } = SidebarLinks();
- 
+
   if (status === "loading") {
     return (
       <div className="flex flex-col-reverse items-center justify-center h-40">
@@ -23,13 +29,13 @@ export default function SideNavbar() {
       </div>
     );
   }
- 
+
   // Determine which links to display based on the current role
   const filteredLinks =
     currentRole === "applicant"
       ? applicantLinks.filter((link) => link.label !== "Get Started")
       : organizationLinks.filter((link) => link.label !== "Get Started");
- 
+
   return (
     <div
       className={cn(
@@ -51,32 +57,46 @@ export default function SideNavbar() {
           />
         </Link>
       </div>
- 
+
       {/* Sidebar links */}
       <div className="flex-grow overflow-y-auto mb-4">
         <Nav isCollapsed={isCollapsed} links={filteredLinks} />
       </div>
- 
+
       {/* Logout button */}
-      <div className="flex justify-center mt-auto px-4">
-        {isCollapsed ? (
-          <span
-            onClick={signOut}
-            className="w-full cursor-pointer flex items-center justify-center"
-          >
-            <span className="sr-only">Logout</span>
-            <LogOut width={16} height={16} />
-          </span>
-        ) : (
-          <button
-            onClick={signOut}
-            className="w-full flex gap-4 cursor-pointer items-center text-xs font-medium ml-4"
-          >
-            <span className="font-medium"><LogOut width={12} height={12} /></span>
-            <span>Logout</span>
-          </button>
-        )}
-      </div>
+      <TooltipProvider>
+        <div className="flex justify-center mt-auto px-4 hover:scale-105 duration-200 dark:text-white">
+          {isCollapsed ? (
+            <Tooltip delayDuration={0}>
+              <TooltipTrigger asChild>
+                <span
+                  onClick={signOut}
+                  className="w-full cursor-pointer flex items-center justify-center dark:hover:bg-muted p-2 rounded-md"
+                >
+                  <span className="sr-only dark:text-gray-200">Logout</span>
+                  <LogOut width={16} height={16} />
+                </span>
+              </TooltipTrigger>
+              <TooltipContent
+                side="right"
+                className="tooltip-content flex items-center gap-4"
+              >
+                <span className="ml-auto text-muted-foreground">Logout</span>
+              </TooltipContent>
+            </Tooltip>
+          ) : (
+            <button
+              onClick={signOut}
+              className="w-full flex gap-4 cursor-pointer items-center text-xs font-medium ml-4 "
+            >
+              <span className="font-medium">
+                <LogOut width={12} height={12} />
+              </span>
+              <span>Logout</span>
+            </button>
+          )}
+        </div>
+      </TooltipProvider>
     </div>
   );
 }
