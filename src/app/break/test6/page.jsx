b9a88dbc9a42d86080e4e43aadcd13c4ef59ce54
emamjion <emamjion@gmail.com>
2025-01-03 23:09:48 +0600

    I install date picker package

--- src/app/break/test6/page.jsx
+++ src/app/break/test6/page.jsx
@@ -1,22 +1,26 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import DatePicker from "./components/DatePicker";
+import DatePickerComponent from "./components/DatePickerComponent";
 
 const DemoProfilePage = () => {
-  const [profileImage, setProfileImage] = useState("");
-  useEffect(() => {
-    const getImageData = async () => {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/applicants`
-      );
-      const data = await response.json();
-      console.log(data);
-    };
-    getImageData();
-  }, []);
+  // const [profileImage, setProfileImage] = useState("");
+  // useEffect(() => {
+  //   const getImageData = async () => {
+  //     const response = await fetch(
+  //       `${process.env.NEXT_PUBLIC_API_URL}/api/applicants`
+  //     );
+  //     const data = await response.json();
+  //     console.log(data);
+  //   };
+  //   getImageData();
+  // }, []);
   return (
     <div>
       <h1>Demo Profile page</h1>
+      <DatePickerComponent />
+
+      {/* <DatePicker /> */}
     </div>
   );
 };
