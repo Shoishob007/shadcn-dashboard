d0a06666db58ba48e803e2908457b97944a7d9ec
emamjion <emamjion@gmail.com>
2025-01-19 10:49:24 +0600

    I added profile tabs in profile demo page

--- src/app/profile-demo/components/Skills.jsx
+++ src/app/profile-demo/components/Skills.jsx
@@ -2,16 +2,28 @@ import { Pencil } from "lucide-react";
 
 const Skills = () => {
   return (
-    <div>
-      <div className="flex items-center justify-between">
-        <h1 className="text-lg font-medium">Skills</h1>
-        <div className="bg-gray-100 hover:bg-gray-200 duration-300 h-8 w-8 flex items-center justify-center rounded-md cursor-pointer">
-          <span className="">
-            <Pencil size={15} />
-          </span>
+    <section className="bg-white dark:bg-gray-800 rounded-lg dark:text-gray-200">
+      <div className="p-4 space-y-4">
+        <div className="flex items-center justify-between">
+          <div className="space-y-2">
+            <h1 className="text-xl font-semibold tracking-tight">Skills</h1>
+            <p className="text-sm text-muted-foreground">
+              The Skills section is your opportunity to share your skills.
+            </p>
+          </div>
+          <div className="bg-gray-100 hover:bg-gray-200 duration-300 h-8 w-8 flex items-center justify-center rounded-md cursor-pointer">
+            <span className="">
+              <Pencil size={15} />
+            </span>
+          </div>
+        </div>
+
+        {/* skills content */}
+        <div>
+          <h1>Skills Content</h1>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
