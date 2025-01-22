7156dc779e0756d89a207899f11c1cd86ed40b62
emamjion <emamjion@gmail.com>
2025-01-22 18:10:31 +0600

    I added job card in test page

---
+++ src/app/break/test3/components/JobCard.jsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader
+} from "@/components/ui/card";
+
+
+const JobCard = ({job}) => {
+    const {orgName, title, location,  skills, employeeType, salary, img} = job;
+    return (
+      <Card className="w-full hover:border hover:border-black duration-300 cursor-pointer">
+        <CardHeader>
+          <div className="flex items-center gap-2">
+            <div className="w-10 h-10 font-medium bg-gray-200 rounded-full flex items-center justify-center">
+            <span>{title.slice(0,1)}</span>
+          </div>
+          <div>
+              <h1 className="text-[15px] font-medium">{orgName}</h1>
+              <p className="text-xs">{location}</p>
+          </div>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div>
+            <h1 className="text-[17px] font-semibold">{title}</h1>
+            <span className={`text-xs font-semibold capitalize ${employeeType === 'full-time' ? 'text-[#20c997]' : employeeType === 'contractual' ? 'text-[#ffc107]' : employeeType === 'part-time' ? 'text-[#6610f2]' : 'text-black'}`}>{employeeType}</span>
+            <div className="flex items-center gap-1 flex-wrap mt-2 text-sm">
+              {
+                skills.map((skill, i) => <span className="text-sm" key={i}>{skill},</span>)
+              }
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <div>
+            <div><span className="font-bold">${salary}</span><span className="text-sm">/month</span></div>
+          </div>
+            <Button size='sm'>View Details</Button>
+        </CardFooter>
+      </Card>
+    );
+};
+
+export default JobCard;
